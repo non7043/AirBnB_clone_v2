ab5f931566479a98e39ad7eac93fbf903e58590f
root <non7043@gmail.com>
2021-12-21 08:34:54 -0800

    AirBnB clone - MySQL

--- models/city.py
+++ models/city.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
+"""This is the city class"""
 from models.base_model import BaseModel, Base
-from models import storage_type
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String, ForeignKey, Integer
 from sqlalchemy.orm import relationship
+from os import environ
+from uuid import uuid4
 
-
-class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    if storage_type == 'db':
-        name = Column(String(128), nullable=False)
+s = "HBNB_TYPE_STORAGE"
+if s in environ.keys() and environ["HBNB_TYPE_STORAGE"] == "db":
+    class City(BaseModel, Base):
+        '''
+        This is the class for City Attributes
+        '''
+        __tablename__ = "cities"
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship('Place', backref='cities',
-                              cascade='all, delete, delete-orphan')
-    else:
-        name = ''
-        state_id = ''
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities", cascade="all,delete")
+
+        def __init__(self, **kwargs):
+            setattr(self, "id", str(uuid4()))
+            for i, j in kwargs.items():
+                setattr(self, i, j)
+else:
+    class City(BaseModel):
+        '''
+        This is the class for City
+        '''
+        state_id = ""
+        name = ""
--- models/user.py
+++ models/user.py
@@ -1,24 +1,36 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""This is the user class"""
 from models.base_model import BaseModel, Base
-from models import storage_type
-from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
-
-class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    if storage_type == 'db':
+s = "HBNB_TYPE_STORAGE"
+if cs in environ.keys() and environ["HBNB_TYPE_STORAGE"] == "db":
+    class User(BaseModel, Base):
+        """This is the class for user
+        Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+        """
+        __tablename__ = "users"
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship('Place', backref='user',
-                              cascade='all, delete, delete-orphan')
-        reviews = relationship('Review', backref='user',
-                               cascade='all, delete, delete-orphan')
-    else:
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+
+        def __init__(self, **kwargs):
+            setattr(self, "id", str(uuid4()))
+            for i, j in kwargs.items():
+                setattr(self, i, j)
+else:
+    class User(BaseModel):
+        """This is the class for user
+        Attributes:
+        """
         email = ""
         password = ""
         first_name = ""
